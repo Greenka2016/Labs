0d06f316383f9f730766f815b8e3b3a5f4edc1aa
Greenka2016 <74435631+Greenka2016@users.noreply.github.com>
2021-01-19 19:08:42 +0500

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+	int a[100];
+	int n;
+	cout << "Длина массива: ";
+	cin >> n;
+	for (int i = 0; i <= n; i++)
+	{
+		a[i] = rand() % 10;
+	}
+	cout << endl;
+	cout << "Массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ' ';
+	}
+	cout << endl << "Удаление элементов с заданным значением:" << endl;
+	cout << "Введите значение: ";
+	int t;
+	int k =0;
+	cin >> t;
+	for (int i = 0; i < n ; i++)
+    if (a[i] == t)
+    {
+        k++;
+    }
+    for (int i = 0; i < k; i++)
+    {
+    n--;
+	for (int i = 0; i < n; i++)
+	{
+    if (a[i] == t)
+        {
+        int change = a[i];
+        a[i] = a[i+1];
+        a[i + 1] = change;
+        }
+	}
+    }
+
+	int temp;
+	int elem;
+    for (int i = 0; i < n; i++)
+	{
+    if (a[i] % 2 == 0)
+    {
+    elem = 0;
+    n++;
+	for (int j = i; j < n; j++)
+	{
+	    temp = a[j];
+	    a[j] = elem;
+	    elem = temp;
+	}
+    i++;
+    }
+	}
+
+	cout << endl;
+    for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+
+    return 0;
+}
